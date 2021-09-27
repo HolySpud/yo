5196558c8442977341e4385bdf4ac86cac5af5ad
HerilS <herilsaha06@gmail.com>
2021-09-27 11:32:18 -0600

    test

--- js/main.js
+++ js/main.js
@@ -51,6 +51,9 @@ bckBtn.addEventListener("click", sceneBackward);
 //   );
 
 // }
+
+let _ = undefined
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -59,7 +62,6 @@ async function dialogue(text, size = "15px", font = "Arial", x, y) {
  let dialogText = "" 
  let strArr = text.split("")
  for (let i = 0; i < strArr.length; i++) {
-	ctx.clearRect(0, 0, cnv.width, cnv.height)
 	dialogText += strArr[i]
 	createText(`${size} ${font}`, dialogText, x, y)
 	await sleep(500)
@@ -67,5 +69,19 @@ async function dialogue(text, size = "15px", font = "Arial", x, y) {
 
 }
 
-dialogue("hello", undefined, undefined, 0, 30);
+async function battleDialog (textArr, size = "15px", font = "Arial", x1, y1, x2, y2) {
+	for (let i = 0; i < textArr.length; i++) {
+		if (i % 2 === 0) {
+			dialogue(textArr[i], size, font, x1, y1)
+		} else {
+			dialogue(textArr[i], size, font, x2, y2)
+		}
+			ctx.clearRect(0, 0, cnv.width, cnv.height)
+		await sleep(500 * textArr.length)
+	}
+}
+
+let textArr = ["hi", "hello", "no u", "why", "why u", 'oh no', 'ida' ]
+battleDialog(textArr, _, _, 0, 30, 100, 30)
+
 
